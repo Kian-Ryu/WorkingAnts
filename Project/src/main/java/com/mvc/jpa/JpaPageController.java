56251d042dfa7dea30f401a680871a7a9dcffbbb
ryu <savewild@naver.com>
2023-04-16 14:39:21 +0900

    태진님의 프로젝트 환경 설정

---
+++ Project/src/main/java/com/mvc/jpa/JpaPageController.java
@@ -0,0 +1,62 @@
+package com.mvc.jpa;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class JpaPageController {
+
+	@GetMapping("/about")
+	public String about() {
+		return "/about";
+	}
+
+	@GetMapping("/blog")
+	public String blog() {
+		return "/blog";
+	}
+
+	@GetMapping("/contact")
+	public String contact() {
+		return "/contact";
+	}
+
+	@GetMapping("/detail")
+	public String detail() {
+		return "/detail";
+	}
+
+	@GetMapping("/feature")
+	public String feature() {
+		return "/feature";
+	}
+
+	@GetMapping("/index")
+	public String index() {
+		return "/index";
+	}
+
+	@GetMapping("/product")
+	public String product() {
+		return "/product";
+	}
+
+	@GetMapping("/service")
+	public String service() {
+		return "/service";
+	}
+
+	@GetMapping("/team")
+	public String team() {
+		return "/team";
+	}
+
+	@GetMapping("/testimonial")
+	public String testimonial() {
+		return "/testimonial";
+	}
+
+
+}
---
+++ Project/src/main/java/com/mvc/jsp/JspPageController.java
@@ -0,0 +1,26 @@
+package com.mvc.jsp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/jsp")
+public class JspPageController {
+
+//	@GetMapping("/forgot-password")
+//	public String forgot() {
+//		return "/member/forgot-password";
+//	}
+//
+//	@GetMapping("/login")
+//	public String login() {
+//		return "/member/login";
+//	}
+//
+//	@GetMapping("/register")
+//	public String register() {
+//		return "/member/register";
+//	}
+
+}
